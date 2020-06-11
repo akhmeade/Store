6f41e9e74ad98afc241ffbdddfb08cc28dea4dbc
Akhmedzianova, Adelina <akhmeade@fel.cvut.cz>
2020-06-11 20:29:30 +0300

    hello

--- spring-onlineStore-service-payments/src/main/java/org/springframework/online/store/payments/model/Payment.java
+++ spring-onlineStore-service-payments/src/main/java/org/springframework/online/store/payments/model/Payment.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+//ergrgrtgrgt
 package org.springframework.online.store.payments.model;
 
 import org.springframework.core.style.ToStringCreator;
