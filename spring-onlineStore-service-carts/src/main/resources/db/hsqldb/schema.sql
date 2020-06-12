210ed22f7551c8abe3fcd60199c69723468a9b9e
regina <mavrireg@fel.cvut.cz>
2020-06-12 15:31:44 +0300

    db

--- spring-onlineStore-service-carts/src/main/resources/db/hsqldb/schema.sql
+++ spring-onlineStore-service-carts/src/main/resources/db/hsqldb/schema.sql
@@ -1,6 +1,20 @@
-DROP TABLE cart IF EXISTS;
+DROP TABLE carts IF EXISTS;
+DROP TABLE types IF EXISTS;
 
-CREATE TABLE cart(
-    id      INTEGER IDENTITY PRIMARY KEY,
-    user_id INTEGER NOT NULL
+CREATE TABLE types (
+  id   INTEGER IDENTITY PRIMARY KEY,
+  name VARCHAR(80)
 );
+CREATE INDEX types_name ON types (name);
+
+CREATE TABLE carts (
+  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  price VARCHAR(255),
+  description VARCHAR(255)
+) engine=InnoDB;
+);
+ALTER TABLE payments ADD CONSTRAINT fk_pets_types FOREIGN KEY (type_id) REFERENCES types (id);
+CREATE INDEX payments_name ON payments (name);
+
+
