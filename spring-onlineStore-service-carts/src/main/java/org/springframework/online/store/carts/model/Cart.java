eb4ac10b6aa9c13064b60863a20fb2353135d4e9
regina <mavrireg@fel.cvut.cz>
2020-06-13 19:51:19 +0300

    +

--- spring-onlineStore-service-carts/src/main/java/org/springframework/online/store/carts/model/Cart.java
+++ spring-onlineStore-service-carts/src/main/java/org/springframework/online/store/carts/model/Cart.java
@@ -18,44 +18,112 @@ package org.springframework.online.store.carts.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.springframework.core.style.ToStringCreator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.Date;
+import java.util.Set;
+import org.springframework.core.style.ToStringCreator;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-/**
- * Simple JavaBean domain object representing a visit.
- *
- * @author Ken Krebs
- * @author Maciej Szarlinski
- */
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+import org.springframework.beans.support.MutableSortDefinition;
+import org.springframework.beans.support.PropertyComparator;
+import org.springframework.core.style.ToStringCreator;
+
+
 @Entity
 @Table(name = "cart")
 @Builder(builderMethodName = "cart")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Cart {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "user_id")
-    private int userId;
+    @Column(name = "name")
+    @NotEmpty
+    private String name;
+
+    @Column(name = "price")
+    @NotEmpty
+    private String price;
+
+    @Column(name = "amount")
+    private Integer amount;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, mappedBy = "owner")
+    private Set<Cart> payments;
 
     public Integer getId() {
         return id;
     }
 
-    public int getUserId() {
-        return userId;
+    public String getName() {
+        return name;
     }
 
-    public Cart setUserId(int userId) {
-        this.userId = userId;
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(final String price) {
+        this.price = price;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public Cart setAmount(Integer amount_) {
+        this.amount = amount_;
         return this;
     }
-}
+
+        @Override
+        public String toString() {
+            return new ToStringCreator(this)
+
+                .append("id", this.getId())
+                .append("name", this.getName())
+                .append("price", this.getPrice())
+                .append("amount", this.getAmount())
+                .toString();
+        }
+    }
