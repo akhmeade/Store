35e79702de090fbe75c631202e5dd4888d81e96a
Dina <latypdin@fel.cvut.cz>
2020-06-13 17:19:18 +0300

    Add button to add new item

--- spring-onlineStore-api-gateway/src/main/resources/static/scripts/item-form/item-form.js
+++ spring-onlineStore-api-gateway/src/main/resources/static/scripts/item-form/item-form.js
@@ -3,11 +3,11 @@
 angular.module('itemForm', ['ui.router'])
     .config(['$stateProvider', function ($stateProvider) {
         $stateProvider
-            .state('petNew', {
-                parent: 'app',
-                url: '/items/:itemId/new-pet',
-                template: '<pet-form></pet-form>'
-            })
+    .state('itemNew', {
+            parent: 'app',
+            url: '/items/new',
+            template: '<item-form></item-form>'
+        })
             .state('itemEdit', {
                 parent: 'app',
                 url: '/items/:itemId/edit',
