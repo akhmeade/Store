ccd8934eb99676638b2cf76f68189771c03f0169
Antonov, Nikolai <antonni1@fel.cvut.cz>
2020-06-14 23:31:27 +0300

    Discounts update

--- spring-onlineStore-service-discounts/src/main/resources/db/mysql/data.sql
+++ spring-onlineStore-service-discounts/src/main/resources/db/mysql/data.sql
@@ -1,26 +1,5 @@
-INSERT IGNORE INTO types VALUES (1, 'CASH');
-INSERT IGNORE INTO types VALUES (2, 'Visa');
-INSERT IGNORE INTO types VALUES (3, 'MasterCard');
-INSERT IGNORE INTO types VALUES (4, 'UnionPay');
-INSERT IGNORE INTO types VALUES (5, 'Mir');
-INSERT IGNORE INTO types VALUES (6, 'China');
-
 INSERT IGNORE INTO discounts VALUES (1, 'Iphone', '10 %', '630 $');
 INSERT IGNORE INTO discounts VALUES (2, 'Iphone', '10 %', '630 $');
 INSERT IGNORE INTO discounts VALUES (3, 'Iphone', '10 %', '630 $');
 INSERT IGNORE INTO discounts VALUES (4, 'Iphone', '10 %', '750 $');
 INSERT IGNORE INTO discounts VALUES (5, 'Iphone', '10 %', '750 $');
-
-INSERT IGNORE INTO payments VALUES (1, 'MasterCard', '2000-09-07', 1, 1);
-INSERT IGNORE INTO payments VALUES (2, 'Visa', '2002-08-06', 6, 2);
-INSERT IGNORE INTO payments VALUES (3, 'Rosy', '2001-04-17', 2, 3);
-INSERT IGNORE INTO payments VALUES (4, 'Jewel', '2000-03-07', 2, 3);
-INSERT IGNORE INTO payments VALUES (5, 'Iggy', '2000-11-30', 3, 4);
-INSERT IGNORE INTO payments VALUES (6, 'George', '2000-01-20', 4, 5);
-INSERT IGNORE INTO payments VALUES (7, 'Samantha', '1995-09-04', 1, 6);
-INSERT IGNORE INTO payments VALUES (8, 'Max', '1995-09-04', 1, 6);
-INSERT IGNORE INTO payments VALUES (9, 'Lucky', '1999-08-06', 5, 7);
-INSERT IGNORE INTO payments VALUES (10, 'Mulligan', '1997-02-24', 2, 8);
-INSERT IGNORE INTO payments VALUES (11, 'Freddy', '2000-03-09', 5, 9);
-INSERT IGNORE INTO payments VALUES (12, 'Lucky', '2000-06-24', 2, 10);
-INSERT IGNORE INTO payments VALUES (13, 'Sly', '2002-06-08', 1, 10);
--- spring-onlineStore-service-discounts/src/main/resources/db/mysql/schema.sql
+++ spring-onlineStore-service-discounts/src/main/resources/db/mysql/schema.sql
@@ -3,26 +3,9 @@ GRANT ALL PRIVILEGES ON onlinestore.* TO pc@localhost IDENTIFIED BY 'pc';
 
 USE onlinestore;
 
-CREATE TABLE IF NOT EXISTS types (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(80),
-  INDEX(name)
-) engine=InnoDB;
-
 CREATE TABLE IF NOT EXISTS discounts (
   id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255),
   discount VARCHAR(255),
   new_price VARCHAR(255)
-) engine=InnoDB;
-
-CREATE TABLE IF NOT EXISTS payments (
-  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30),
-  some_date DATE,
-  type_id INT(4) UNSIGNED NOT NULL,
-  owner_id INT(4) UNSIGNED NOT NULL,
-  INDEX(name),
-  FOREIGN KEY (owner_id) REFERENCES items(id),
-  FOREIGN KEY (type_id) REFERENCES types(id)
 ) engine=InnoDB;
